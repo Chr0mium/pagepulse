76456f557c1534874770928d4ebb32e9b09277e4
Shubhanshu Shubham <explorewithshubhanshu@gmail.com>
2026-07-31 01:04:50 +0530

    updated server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
-app.listen(PORT, () => {
-  console.log(`PagePulse API running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`PagePulse API running on port ${PORT}`);
 });
